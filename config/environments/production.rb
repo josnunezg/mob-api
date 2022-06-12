e2bd120e1f1a0ccd57916812cf36ba86c44b8276
Jose Nuñez <josnunez@MacBook-Pro-de-Jose.local>
2022-06-11 23:36:46 -0400

    all hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.config.hosts << ENV['FRONT_APP_URL']
+  config.hosts.clear
 end
